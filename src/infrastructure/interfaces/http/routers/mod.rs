6db8a0b247cec2bd4cbff8dbb7ce8669e9ff216f
cainhooow <augustocaio663@gmail.com>
2025-09-04 14:53:21 -0300

    Rotas modularizadas usando cascading routes

--- src/infrastructure/interfaces/http/routers/mod.rs
+++ src/infrastructure/interfaces/http/routers/mod.rs
@@ -1,9 +1,8 @@
 use salvo::prelude::*;
-pub mod product_router;
-use self::product_router::product_router;
+pub mod product;
 
 pub fn routers() -> Router {
     Router::with_path("v1")
         .hoop(Logger::new())
-        .push(product_router())
+        .push(product::router())
 }
--- src/infrastructure/interfaces/http/routers/product.rs
+++ src/infrastructure/interfaces/http/routers/product.rs
@@ -4,9 +4,9 @@ use crate::infrastructure::interfaces::http::handlers::product_handler::{
     create_product_handler, get_product_by_id_handler, index_product_handler,
 };
 
-pub fn product_router() -> Router {
+pub fn router() -> Router {
     Router::with_path("products")
         .get(index_product_handler)
         .post(create_product_handler)
-        .push(Router::with_path("/{id}").get(get_product_by_id_handler))
+        .push(Router::with_path("{id}").get(get_product_by_id_handler))
 }
