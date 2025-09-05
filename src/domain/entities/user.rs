e5fba80802cd0fb0cf99e31cd7174283b3c53cf4
cainhooow <augustocaio663@gmail.com>
2025-09-05 00:37:53 -0300

    User builder

---
+++ src/domain/entities/user.rs
@@ -0,0 +1,42 @@
+#[derive(Debug, Clone)]
+pub struct User {
+    pub id: Option<i32>,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+pub struct UserBuilder {
+    name: String,
+    email: String,
+    password: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct NewUser {
+    name: String,
+    email: String,
+    password: String,
+}
+
+impl UserBuilder {
+    pub fn new(
+        name: impl Into<String>,
+        email: impl Into<String>,
+        password: impl Into<String>,
+    ) -> Self {
+        Self {
+            name: name.into(),
+            email: email.into(),
+            password: password.into(),
+        }
+    }
+
+    pub fn build(self) -> NewUser {
+        NewUser {
+            name: self.name,
+            email: self.email,
+            password: self.password,
+        }
+    }
+}
