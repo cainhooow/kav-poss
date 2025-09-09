56dcdfe2af8dda69de35175317288bfc7d465484
cainhooow <augustocaio663@gmail.com>
2025-09-09 11:33:57 -0300

    remover trait Mapper

--- src/infrastructure/mappers/mod.rs
+++ src/infrastructure/mappers/mod.rs
@@ -2,7 +2,7 @@ pub mod product_mapper;
 pub mod role_mapper;
 pub mod user_mapper;
 
-pub trait Mapper<I, D> {
-    fn to_domain(string: String) -> D;
-    fn to_infra(domain: D) -> I;
-}
+// pub trait Mapper<I, D> {
+//     fn to_domain(string: String) -> D;
+//     fn to_infra(domain: D) -> I;
+// }
