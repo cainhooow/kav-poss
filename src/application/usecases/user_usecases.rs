6aeb13dd955830daf1c7ddb4ab908f48f214e408
cainhooow <augustocaio663@gmail.com>
2025-09-15 10:53:46 -0300

    create user definido como unico em usecases

--- src/application/usecases/user_usecases.rs
+++ src/application/usecases/user_usecases.rs
@@ -1,4 +1,4 @@
-use argon2::{password_hash::{Salt, SaltString}, Argon2, PasswordHasher, PasswordVerifier};
+use argon2::{Argon2, PasswordHasher, password_hash::SaltString};
 use core_server::RoleEnum;
 use crypto::common::rand_core::OsRng;
 
@@ -6,36 +6,17 @@ use crate::{
     application::exceptions::AppResult,
     domain::{
         builders::user_builder::UserBuilder,
-        entities::{user::User},
+        entities::user::User,
         repositories::{
             role_repository_interface::RoleRepository, user_repository_interface::UserRepository,
         },
     },
 };
 
-// pub struct CreateUserUseCase<R: UserRepository> {
-//     repository: R,
-// }
-
 pub struct CreateUserWithRolesUseCase<U: UserRepository, R: RoleRepository> {
     user_repository: U,
     role_repository: R,
 }
-
-// impl<R: UserRepository> CreateUserUseCase<R> {
-//     pub fn new(repo: R) -> Self {
-//         Self { repository: repo }
-//     }
-
-//     pub async fn execute(&self, name: String, email: String, password: &str) -> AppResult<User> {
-//         let argon2 = Argon2::default();
-//         let salt: Salt = password.try_into().unwrap();
-//         let password = argon2.hash_password(password.as_bytes(), salt)?;
-
-//         let user = UserBuilder::new(name, email, password.to_string()).build();
-//         Ok(self.repository.save(&user).await?)
-//     }
-// }
 
 impl<U: UserRepository, R: RoleRepository> CreateUserWithRolesUseCase<U, R> {
     pub fn new(user_repository: U, role_repository: R) -> Self {
