008fcdaefb79448194088671f772f56c21b4cd56
cainhooow <augustocaio663@gmail.com>
2026-01-24 12:37:41 -0300

    Impl Queries Simples para Company

--- src/application/queries/company_query.rs
+++ src/application/queries/company_query.rs
@@ -1,1 +1,34 @@
+use crate::{
+    application::exceptions::AppResult,
+    domain::{entities::company::Company, repositories::company_repository::CompanyRepository},
+};
 
+pub struct FindCompanyById<R: CompanyRepository> {
+    repository: R,
+}
+
+impl<R: CompanyRepository> FindCompanyById<R> {
+    pub fn new(repo: R) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute(&self, id: i32) -> AppResult<Company> {
+        let company = self.repository.find_by_id(id).await?;
+        Ok(company)
+    }
+}
+
+pub struct FindCompanyByUserId<R: CompanyRepository> {
+    repository: R,
+}
+
+impl<R: CompanyRepository> FindCompanyByUserId<R> {
+    pub fn new(repo: R) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute(&self, user_id: i32) -> AppResult<Company> {
+        let company = self.repository.find_by_user_id(user_id).await?;
+        Ok(company)
+    }
+}
