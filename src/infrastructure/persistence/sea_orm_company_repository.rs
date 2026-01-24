fced3223a0658e20ad0c5feb28bbad9c1b9ef6a5
cainhooow <augustocaio663@gmail.com>
2026-01-24 12:30:21 -0300

    Impl Infra Repository para Company

--- src/infrastructure/persistence/sea_orm_company_repository.rs
+++ src/infrastructure/persistence/sea_orm_company_repository.rs
@@ -1,1 +1,46 @@
+use std::sync::Arc;
 
+use sea_orm::DatabaseConnection;
+
+use crate::domain::{
+    entities::company::{Company, NewCompany},
+    exceptions::RepositoryError,
+    repositories::company_repository::CompanyRepository,
+};
+
+pub struct SeaOrmCompanyRepository {
+    conn: Arc<DatabaseConnection>,
+}
+
+impl SeaOrmCompanyRepository {
+    pub fn new(conn: Arc<DatabaseConnection>) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait::async_trait]
+impl CompanyRepository for SeaOrmCompanyRepository {
+    async fn save(&self, company: &NewCompany) -> Result<Company, RepositoryError> {
+        Ok(Company {
+            id: Some(15),
+            name: String::from("Company"),
+            user_id: 1,
+        })
+    }
+
+    async fn find_by_id(&self, id: i32) -> Result<Company, RepositoryError> {
+        Ok(Company {
+            id: Some(id),
+            name: String::from("Company"),
+            user_id: 15,
+        })
+    }
+
+    async fn find_by_user_id(&self, user_id: i32) -> Result<Company, RepositoryError> {
+        Ok(Company {
+            id: Some(21),
+            name: String::from("Company"),
+            user_id,
+        })
+    }
+}
