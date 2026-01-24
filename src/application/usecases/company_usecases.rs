7f7abf664979e5e778bb1b0d783a517603ce2cb4
cainhooow <augustocaio663@gmail.com>
2026-01-24 12:33:48 -0300

    Impl UseCase para Company

--- src/application/usecases/company_usecases.rs
+++ src/application/usecases/company_usecases.rs
@@ -1,1 +1,23 @@
+use crate::{
+    application::exceptions::AppResult,
+    domain::{
+        builders::company_builder::CompanyBuilder, entities::company::Company,
+        repositories::company_repository::CompanyRepository,
+    },
+};
 
+pub struct CreateCompanyUseCase<R: CompanyRepository> {
+    repository: R,
+}
+
+impl<R: CompanyRepository> CreateCompanyUseCase<R> {
+    pub fn new(repo: R) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute(&self, name: String, user_id: i32) -> AppResult<Company> {
+        let company = CompanyBuilder::new(name, user_id).build();
+
+        Ok(self.repository.save(&company).await?)
+    }
+}
