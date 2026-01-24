4baab537ddfba805633393cf845a059ba064072c
cainhooow <augustocaio663@gmail.com>
2026-01-24 12:20:27 -0300

    Domain Repository para Company

--- src/domain/repositories/company_repository.rs
+++ src/domain/repositories/company_repository.rs
@@ -1,1 +1,8 @@
+use crate::domain::{entities::company::{Company, NewCompany}, exceptions::RepositoryError};
 
+#[async_trait::async_trait]
+pub trait CompanyRepository: Send + Sync {
+    async fn save(&self, company: &NewCompany) -> Result<Company, RepositoryError>;
+    async fn find(&self, id: i32) -> Result<Company, RepositoryError>;
+    async fn find_by_user_id(&self, user_id: i32) -> Result<Company, RepositoryError>;
+}
