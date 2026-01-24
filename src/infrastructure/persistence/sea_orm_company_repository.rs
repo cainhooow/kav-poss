11546c47b2a83fa165fe3e8fc356c56b2cda042c
cainhooow <augustocaio663@gmail.com>
2026-01-24 12:52:31 -0300

    SaveModel para Company Repository

--- src/infrastructure/persistence/sea_orm_company_repository.rs
+++ src/infrastructure/persistence/sea_orm_company_repository.rs
@@ -1,12 +1,13 @@
-use std::sync::Arc;
-
-use sea_orm::DatabaseConnection;
-
-use crate::domain::{
-    entities::company::{Company, NewCompany},
-    exceptions::RepositoryError,
-    repositories::company_repository_interface::CompanyRepository,
+use crate::{
+    domain::{
+        entities::company::{Company, NewCompany},
+        exceptions::RepositoryError,
+        repositories::company_repository_interface::CompanyRepository,
+    },
+    infrastructure::entities::company::{self as CompanyModel, Entity as CompanyEntity},
 };
+use sea_orm::{ActiveModelTrait, ActiveValue::Set, DatabaseConnection};
+use std::sync::Arc;
 
 pub struct SeaOrmCompanyRepository {
     conn: Arc<DatabaseConnection>,
@@ -21,11 +22,16 @@ impl SeaOrmCompanyRepository {
 #[async_trait::async_trait]
 impl CompanyRepository for SeaOrmCompanyRepository {
     async fn save(&self, company: &NewCompany) -> Result<Company, RepositoryError> {
-        Ok(Company {
-            id: Some(15),
-            name: String::from("Company"),
-            user_id: 1,
-        })
+        let model = CompanyModel::ActiveModel {
+            name: Set(company.name.clone()),
+            user_id: Set(company.user_id.clone()),
+            ..Default::default()
+        };
+
+        match model.insert(&*self.conn).await {
+            Ok(data) => Ok(Company::from(data)),
+            Err(err) => Err(RepositoryError::Generic(err.to_string())),
+        }
     }
 
     async fn find_by_id(&self, id: i32) -> Result<Company, RepositoryError> {
