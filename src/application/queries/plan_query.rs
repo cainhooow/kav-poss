5843c51bbbea7a93db77739b8c50bdb2c7bced48
cainhooow <augustocaio663@gmail.com>
2026-02-02 11:01:07 -0300

    Plan UseCase e Plan Query

--- src/application/queries/plan_query.rs
+++ src/application/queries/plan_query.rs
@@ -1,1 +1,19 @@
+use crate::{
+    application::exceptions::AppResult,
+    domain::{entities::plan::Plan, repositories::plan_repository_interface::PlanRepository},
+};
 
+pub struct FindPlanByIdQuery<R: PlanRepository> {
+    repository: R,
+}
+
+impl<R: PlanRepository> FindPlanByIdQuery<R> {
+    pub fn new(repo: R) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute(&self, id: i32) -> AppResult<Plan> {
+        let plan = self.repository.find_by_id(id).await?;
+        Ok(plan)
+    }
+}
--- src/application/usecases/plan_usecases.rs
+++ src/application/usecases/plan_usecases.rs
@@ -1,1 +1,30 @@
+use crate::{
+    application::exceptions::AppResult,
+    domain::{
+        builders::plan_builder::PlanBuilder, entities::plan::Plan,
+        repositories::plan_repository_interface::PlanRepository,
+    },
+};
 
+pub struct CreatePlanUseCase<R: PlanRepository> {
+    repository: R,
+}
+
+impl<R: PlanRepository> CreatePlanUseCase<R> {
+    pub fn new(repo: R) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute(
+        &self,
+        name: String,
+        price: i32,
+        description: Option<String>,
+    ) -> AppResult<Plan> {
+        let plan = PlanBuilder::new(name, price)
+            .description(description)
+            .build();
+
+        Ok(self.repository.save(&plan).await?)
+    }
+}
