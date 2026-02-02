e4ed6014d60949ec23ff9134de8847d64c5470fa
cainhooow <augustocaio663@gmail.com>
2026-02-02 11:51:00 -0300

    PlanResource e PlanRequest

--- src/infrastructure/interfaces/http/resources/plan_resource.rs
+++ src/infrastructure/interfaces/http/resources/plan_resource.rs
@@ -1,1 +1,44 @@
+use garde::{Valid, Validate};
+use serde::{Deserialize, Serialize};
 
+use crate::domain::entities::plan::Plan;
+
+#[derive(Serialize, Deserialize, Validate)]
+pub struct PlanRequest {
+    #[garde(ascii, length(min = 5))]
+    pub name: String,
+    #[garde(range(min = 0))]
+    pub price: i32,
+    #[garde(ascii, length(min = 5, max = 1000), required)]
+    pub description: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PlanResource {
+    pub id: Option<i32>,
+    pub name: String,
+    pub description: Option<String>,
+    pub price: i32,
+}
+
+impl From<&Plan> for PlanResource {
+    fn from(value: &Plan) -> Self {
+        Self {
+            id: value.id.clone(),
+            name: value.name.clone(),
+            description: value.description.clone(),
+            price: value.price.clone(),
+        }
+    }
+}
+
+impl From<Plan> for PlanResource {
+    fn from(value: Plan) -> Self {
+        Self {
+            id: value.id,
+            name: value.name,
+            description: value.description,
+            price: value.price,
+        }
+    }
+}
