ae0b40a71a13cad7ed194bdbcf5c348eaae6daa3
cainhooow <augustocaio663@gmail.com>
2026-02-10 10:39:25 -0300

    Estrutura de tabela CompanyColaborator

--- migration/src/m20260209_133932_create_company_colaborators_table.rs
+++ migration/src/m20260209_133932_create_company_colaborators_table.rs
@@ -1,5 +1,9 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20250904_211710_create_users_table::User, m20260123_222500_create_companies_table::Company,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
@@ -7,16 +11,47 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+
+        let mut company_fk = ForeignKey::create()
+            .from(CompanyColaborator::Table, CompanyColaborator::CompanyId)
+            .to(Company::Table, Company::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut user_fk = ForeignKey::create()
+            .from(CompanyColaborator::Table, CompanyColaborator::UserId)
+            .to(User::Table, User::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
 
         manager
             .create_table(
                 Table::create()
-                    .table("post")
+                    .table(CompanyColaborator::Table)
                     .if_not_exists()
-                    .col(pk_auto("id"))
-                    .col(string("title"))
-                    .col(string("text"))
+                    .col(pk_auto(CompanyColaborator::Id))
+                    .col(
+                        ColumnDef::new(CompanyColaborator::CompanyId)
+                            .integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(CompanyColaborator::UserId)
+                            .integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(CompanyColaborator::Document)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(CompanyColaborator::Badge)
+                            .string()
+                            .not_null(),
+                    )
+                    .foreign_key(&mut company_fk)
+                    .foreign_key(&mut user_fk)
                     .to_owned(),
             )
             .await
@@ -24,10 +59,18 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table("post").to_owned())
+            .drop_table(Table::drop().table(CompanyColaborator::Table).to_owned())
             .await
     }
+}
+
+#[derive(DeriveIden)]
+pub enum CompanyColaborator {
+    Id,
+    Table,
+    CompanyId,
+    UserId,
+    Document,
+    Badge,
 }
