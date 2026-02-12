9d722561fc2fc2983ce53ee497854d0ee69f95b4
cainhooow <augustocaio663@gmail.com>
2026-02-12 09:27:27 -0300

    CompanyRole builder e Domain Entity

---
+++ src/domain/entities/company_role.rs
@@ -0,0 +1,43 @@
+pub struct CompanyRole {
+    pub id: Option<i32>,
+    pub name: String,
+    pub description: Option<String>,
+    pub company_id: i32,
+}
+
+pub struct CompanyRoleBuilder {
+    name: String,
+    description: Option<String>,
+    company_id: i32,
+}
+
+pub struct NewCompanyRole {
+    pub name: String,
+    pub description: String,
+    pub company_id: i32,
+}
+
+impl CompanyRoleBuilder {
+    pub fn new(name: impl Into<String>, company_id: i32) -> Self {
+        Self {
+            name: name.into(),
+            description: None,
+            company_id: company_id,
+        }
+    }
+
+    pub fn name(mut self, name: impl Into<String>) -> Self {
+        self.name = name.into();
+        self
+    }
+
+    pub fn description(mut self, description: impl Into<String>) -> Self {
+        self.description = self.description;
+        self
+    }
+
+    pub fn company(mut self, company_id: i32) -> Self {
+        self.company_id = company_id;
+        self
+    }
+}
--- src/domain/entities/mod.rs
+++ src/domain/entities/mod.rs
@@ -1,5 +1,6 @@
 pub mod company;
 pub mod company_colaborator;
+pub mod company_role;
 pub mod plan;
 pub mod product;
 pub mod role;
