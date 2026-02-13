72a1b860ef62d38b391149232c857cf6a83d5352
cainhooow <augustocaio663@gmail.com>
2026-02-13 10:35:11 -0300

    Implementação para company_role repository

--- src/domain/repositories/company_role_repository_interface.rs
+++ src/domain/repositories/company_role_repository_interface.rs
@@ -7,7 +7,7 @@ use crate::domain::{
 
 #[async_trait::async_trait]
 pub trait CompanyRoleRepository: Send + Sync {
-    async fn save(&self, role: NewCompanyRole) -> Result<CompanyRole, RepositoryError>;
+    async fn save(&self, role: &NewCompanyRole) -> Result<CompanyRole, RepositoryError>;
     async fn all(&self) -> Result<Vec<CompanyRole>, RepositoryError>;
     async fn find_by_id(&self, role_id: i32) -> Result<CompanyRole, RepositoryError>;
 }
--- src/infrastructure/persistence/sea_orm_company_role_repository.rs
+++ src/infrastructure/persistence/sea_orm_company_role_repository.rs
@@ -1,1 +1,49 @@
+use std::sync::Arc;
 
+use sea_orm::DatabaseConnection;
+
+use crate::domain::{
+    entities::company_role::{CompanyRole, NewCompanyRole},
+    exceptions::RepositoryError,
+    repositories::company_role_repository_interface::CompanyRoleRepository,
+};
+
+pub struct SeaOrmCompanyRoleRepository {
+    conn: Arc<DatabaseConnection>,
+}
+
+impl SeaOrmCompanyRoleRepository {
+    pub fn new(conn: Arc<DatabaseConnection>) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait::async_trait]
+impl CompanyRoleRepository for SeaOrmCompanyRoleRepository {
+    async fn save(&self, role: &NewCompanyRole) -> Result<CompanyRole, RepositoryError> {
+        Ok(CompanyRole {
+            id: Some(2),
+            name: String::from("value"),
+            description: Some(String::from("value")),
+            company_id: 10,
+        })
+    }
+
+    async fn all(&self) -> Result<Vec<CompanyRole>, RepositoryError> {
+        Ok(vec![CompanyRole {
+            id: Some(2),
+            name: String::from("value"),
+            description: Some(String::from("value")),
+            company_id: 10,
+        }])
+    }
+
+    async fn find_by_id(&self, role_id: i32) -> Result<CompanyRole, RepositoryError> {
+        Ok(CompanyRole {
+            id: Some(2),
+            name: String::from("value"),
+            description: Some(String::from("value")),
+            company_id: 10,
+        })
+    }
+}
