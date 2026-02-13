d057a5a1af823801968d820e2c51d1237ad45489
cainhooow <augustocaio663@gmail.com>
2026-02-13 11:20:31 -0300

    Alterar metodo de conexão

--- src/infrastructure/database/mod.rs
+++ src/infrastructure/database/mod.rs
@@ -5,7 +5,10 @@ use sea_orm::{Database, DatabaseConnection};
 pub async fn estabilish_connection() -> DatabaseConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL NOT PROVIDED");
 
-    Database::connect(&database_url)
-        .await
-        .expect("Failed to connect database")
+    match Database::connect(&database_url).await {
+        Ok(connection) => connection,
+        Err(err) => {
+            panic!("Failed to connect database {:?}", err.to_string())
+        }
+    }
 }
