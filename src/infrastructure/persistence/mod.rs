85d6e0103e9202744359c400c45405db4a08aba2
cainhooow <augustocaio663@gmail.com>
2026-02-13 10:17:54 -0300

    Estrutura sea-orm para company_role e company_colaborator

--- src/infrastructure/persistence/mod.rs
+++ src/infrastructure/persistence/mod.rs
@@ -1,5 +1,7 @@
 pub mod exceptions;
+pub mod sea_orm_colaborator_repository;
 pub mod sea_orm_company_repository;
+pub mod sea_orm_company_role_repository;
 pub mod sea_orm_plan_repository;
 pub mod sea_orm_product_repository;
 pub mod sea_orm_role_repository;
