32b1ef3a279a2be105bc6032a991e236920d8702
cainhooow <augustocaio663@gmail.com>
2026-02-13 10:07:24 -0300

    CompanyColaborator Repository

--- src/domain/repositories/colaborator_repository_interface.rs
+++ src/domain/repositories/colaborator_repository_interface.rs
@@ -1,1 +1,14 @@
+use crate::domain::{
+    entities::company_colaborator::{CompanyColaborator, NewColaborator},
+    exceptions::RepositoryError,
+};
 
+#[async_trait::async_trait]
+pub trait ColaboratorRepository: Send + Sync {
+    async fn save(
+        &self,
+        colaborator: &NewColaborator,
+    ) -> Result<CompanyColaborator, RepositoryError>;
+    async fn all(&self) -> Result<Vec<CompanyColaborator>, RepositoryError>;
+    async fn find_by_id(&self, colaorator_id: i32) -> Result<CompanyColaborator, RepositoryError>;
+}
